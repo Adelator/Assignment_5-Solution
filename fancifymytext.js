0f5f18b42fdbaef1f038d760a4cdcbbf965063d8
Adelator <146053462+Adelator@users.noreply.github.com>
2026-05-14 15:48:35 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,35 @@
+function makeBigger() {
+    document.getElementById("textArea").style.fontSize = "24pt";
+}
+
+function fancifyText() {
+    let textArea = document.getElementById("textArea");
+
+    if (document.getElementById("fancy").checked) {
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    } else if (document.getElementById("boring").checked) {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+}
+
+function mooText() {
+    let textArea = document.getElementById("textArea");
+
+    let text = textArea.value.toUpperCase();
+
+    let sentences = text.split(".");
+    for (let i = 0; i < sentences.length - 1; i++) {
+        let words = sentences[i].trim().split(" ");
+
+        if (words.length > 0 && words[0] !== "") {
+            words[words.length - 1] = words[words.length - 1] + "-Moo";
+            sentences[i] = words.join(" ");
+        }
+    }
+
+    textArea.value = sentences.join(".");
+}
